b808cda37b83a68d31537810b6e77861394add62
laladwesh <guptaavinash302@gmail.com>
2026-07-23 22:48:35 +0530

    feat: integrate Google OAuth and AuthProvider into App component

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import ScrollToTop from "./components/ScrollToTop";
 import { CartProvider } from "./context/CartContext";
+import { AuthProvider } from "./context/AuthContext";
 import Home from "./pages/Home";
 import ServicesPage from "./pages/ServicesPage";
 import ShopPage from "./pages/ShopPage";
@@ -20,35 +22,43 @@ import ProductListingPage from "./pages/ProductListingPage";
 import ProductDetailPage from "./pages/ProductDetailPage";
 import CartPage from "./pages/CartPage";
 import CheckoutPage from "./pages/CheckoutPage";
+import MyAccountPage from "./pages/MyAccountPage";
+
+const GOOGLE_CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID || "";
 
 function App() {
   return (
-    <CartProvider>
-      <BrowserRouter>
-        <ScrollToTop />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/services" element={<ServicesPage />} />
-          <Route path="/shop" element={<ShopPage />} />
-          <Route path="/calculators" element={<CalculatorsPage />} />
-          <Route path="/gallery" element={<GalleryPage />} />
-          <Route path="/about-us" element={<AboutUsPage />} />
-          <Route path="/resources" element={<ResourcesPage />} />
-          <Route path="/contact-us" element={<ContactPage />} />
-          <Route path="/request-a-quote" element={<RequestQuotePage />} />
-          <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
-          <Route path="/blog" element={<BlogPage />} />
-          <Route path="/blog/:slug" element={<ArticlePage />} />
-          <Route path="/calculators/fence-calculator" element={<FenceCalculatorPage />} />
-          <Route path="/calculators/retaining-calculator" element={<RetainingCalculatorPage />} />
-          <Route path="/faqs" element={<FaqPage />} />
-          <Route path="/shop/:categorySlug" element={<ProductListingPage />} />
-          <Route path="/product/:slug" element={<ProductDetailPage />} />
-          <Route path="/cart" element={<CartPage />} />
-          <Route path="/checkout" element={<CheckoutPage />} />
-        </Routes>
-      </BrowserRouter>
-    </CartProvider>
+    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+      <AuthProvider>
+        <CartProvider>
+          <BrowserRouter>
+            <ScrollToTop />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/services" element={<ServicesPage />} />
+              <Route path="/shop" element={<ShopPage />} />
+              <Route path="/calculators" element={<CalculatorsPage />} />
+              <Route path="/gallery" element={<GalleryPage />} />
+              <Route path="/about-us" element={<AboutUsPage />} />
+              <Route path="/resources" element={<ResourcesPage />} />
+              <Route path="/contact-us" element={<ContactPage />} />
+              <Route path="/request-a-quote" element={<RequestQuotePage />} />
+              <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
+              <Route path="/blog" element={<BlogPage />} />
+              <Route path="/blog/:slug" element={<ArticlePage />} />
+              <Route path="/calculators/fence-calculator" element={<FenceCalculatorPage />} />
+              <Route path="/calculators/retaining-calculator" element={<RetainingCalculatorPage />} />
+              <Route path="/faqs" element={<FaqPage />} />
+              <Route path="/shop/:categorySlug" element={<ProductListingPage />} />
+              <Route path="/product/:slug" element={<ProductDetailPage />} />
+              <Route path="/cart" element={<CartPage />} />
+              <Route path="/checkout" element={<CheckoutPage />} />
+              <Route path="/my-account" element={<MyAccountPage />} />
+            </Routes>
+          </BrowserRouter>
+        </CartProvider>
+      </AuthProvider>
+    </GoogleOAuthProvider>
   );
 }
 
