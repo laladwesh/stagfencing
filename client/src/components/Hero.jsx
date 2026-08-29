e81c56fa6ad9544a983e99ad4910c7cfc216bd49
laladwesh <guptaavinash302@gmail.com>
2026-08-29 20:22:46 +0530

    feat: update Hero component background image URL

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -39,7 +39,7 @@ function Hero() {
         className="relative bg-cover bg-center"
         style={{
           backgroundImage:
-            "linear-gradient(180deg, rgba(20,15,10,0.35) 0%, rgba(20,15,10,0.55) 60%, rgba(20,15,10,0.85) 100%), url('https://stagfencing-media.s3.ap-southeast-2.amazonaws.com/site/1788013878512-978305ff74033ae7.png')",
+            "linear-gradient(180deg, rgba(20,15,10,0.35) 0%, rgba(20,15,10,0.55) 60%, rgba(20,15,10,0.85) 100%), url('https://stagfencing-media.s3.ap-southeast-2.amazonaws.com/site/1788015097429-e11404b3b4ef871c.png')",
           backgroundColor: "#3a3226",
         }}
       >
